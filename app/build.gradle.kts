3ce0cf9c027edf8271e2b416085e3eaee7968fd7
ruyue <ruyueee@outlook.com>
2026-05-10 19:02:40 +0800

    About: Add donate and communtiy page
    using compose
    restore compose dependencies
    compose is right
    
    Change-Id: I5650c623d84bc2f51416c997250ead54b8215197

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,12 +33,28 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.10"
+    }
 }
 
 dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
